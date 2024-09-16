2e09dccf2c5b3d479321f0de5a1ac74d39e35908
seoyoung927 <seoyoung0000@naver.com>
2024-09-16 10:17:27 +0900

    :sparkles: [Feat] 없어진 기록 찾기

---
+++ 없어진기록찾기.sql
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/59042?language=mysql
+select
+    ao.animal_id as ANIMAL_ID,
+    ao.name as NAME
+from animal_outs ao
+left join animal_ins ai
+on ao.animal_id = ai.animal_id
+where ai.animal_id is null
+order by ao.animal_id;
